695eb7cb38697a75f63e10d60309f964cbe0ab27
happyeric77 <happyeric77@gmail.com>
2022-01-10 12:05:51 +0900

    Enable wallectconnect

--- App.tsx
+++ App.tsx
@@ -1,34 +1,19 @@
-import AppLoading from 'expo-app-loading';
-import { useState } from 'react';
-import * as Font from "expo-font";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Main from "./screens/Main"
-import Second from './screens/Second';
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'bank-gothic-light': require('./assets/fonts/bank-gothic-light-bt.ttf')
-  })
-}
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import { View, Button } from 'react-native';
 
 export default function App() {
-  
-  const [initFonts, setInitFonts] = useState<boolean>(false)
-
-  const Stake = createNativeStackNavigator()
-
-  if (!initFonts) {
-    return <AppLoading startAsync={fetchFonts} onFinish={()=>setInitFonts(true)} onError={console.log}/>
+    const connector = useWalletConnect()
+    return (
+      <View>
+        <Button title="OK" onPress={async()=>{
+          // connector.connect(); 
+          console.log(await connector.connect())
+          }}/>
+        <Button title="Disconnect" onPress={async()=>{
+          // connector.connect(); 
+          console.log(await connector.killSession())
+          }}/>
+      </View>
+    );
   }
-  
-  return (    
-    <NavigationContainer>
-      <Stake.Navigator>
-        <Stake.Screen name="Home Page" component={Main} />
-        <Stake.Screen name="Second Page" component={Second} />
-      </Stake.Navigator>
-    </NavigationContainer>
-  );
-}
-
--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import Navigation from '../typs';
-
+import Navigation from '../types';
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
 
 export default function Main({navigation}: any) {
+  const connector = useWalletConnect()
   return (
       <View style={styles.container}>
-      <Text style={styles.textContent}>This is the Main </Text>
-      <Button onPress={() => {(navigation as Navigation).navigate('Second Page')}} title="Go to Second Page"/>
-      <StatusBar style="auto" />
+        <Text style={styles.textContent}>This is the Main </Text>
+        <Button onPress={() => {(navigation as Navigation).navigate('Second Page')}} title="Go to Second Page"/>
+        <Button onPress={()=> {connector.connect()}} title="CONNECT"/>
+        <StatusBar style="auto" />
       </View>
   );
 }
