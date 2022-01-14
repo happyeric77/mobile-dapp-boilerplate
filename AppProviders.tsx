79b6792e9cfc7c62ebc4979863b8eb3f4a497e2c
happyeric77 <happyeric77@gmail.com>
2022-01-14 16:03:04 +0900

    Init project

--- AppProviders.tsx
+++ AppProviders.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import WalletConnectProvider, { WalletConnectProviderProps } from "@walletconnect/react-native-dapp";
+// import WalletConnectProvider, { WalletConnectProviderProps } from "@walletconnect/react-native-dapp";
+import WalletConnectProvider, { WalletConnectProviderProps } from "./WalletConnect";
+import { enableMoralisViaWalletConnect } from "./enableMoralisViaMoralis";
 import { Platform} from "react-native";
 import { expo } from "./app.json";
 const { scheme } = expo;
@@ -8,10 +10,11 @@ import App from "./App";
 
 import { MoralisProvider } from "react-moralis"
 
-
-
 import Moralis from "moralis/react-native.js";
 Moralis.setAsyncStorage(AsyncStorage)
+//@ts-ignore
+Moralis.enable = enableMoralisViaWalletConnect
+console.log(AsyncStorage.getAllKeys(), "KEYS");
 
 const walletConnectOptions: WalletConnectProviderProps = {
     redirectUrl: Platform.OS === "web" ? window.location.origin : `${scheme}://`,
@@ -40,9 +43,8 @@ export const AppProviders = ({ children }: any) => {
             appId={process.env.REACT_APP_MORALIS_APPLICATION_ID!}
             serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL!}
             environment="native">
-            <App/>
+                <App/>
             </MoralisProvider>
         </WalletConnectProvider>
-        
     );
 };
--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,27 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Navigation from '../types';
-import { useWalletConnect } from '@walletconnect/react-native-dapp';
-import { getWeb3 } from '../getWeb3';
+// import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import { useWalletConnect } from '../WalletConnect';
 import { useMoralis} from "react-moralis";
+import { enableMoralisViaWalletConnect } from '../enableMoralisViaMoralis';
 
 export default function Main({navigation}: any) {
-  const {isAuthenticated, user} = useMoralis()
+  const { web3, Moralis, user, authenticate, authError, isAuthenticated } = useMoralis();
   const connector = useWalletConnect()
 
   return (
       <View style={styles.container}>
-        <Text style={styles.textContent}>This is the Main {JSON.stringify(isAuthenticated)} {JSON.stringify(user)} </Text>
+        <Text style={styles.textContent}>This is the Main  {JSON.stringify(user)} </Text>
         <Button onPress={() => {(navigation as Navigation).navigate('Second Page')}} title="Go to Second Page"/>
-        <Button onPress={()=> {
-          connector.connect()
+        <Button onPress={async ()=> {
+          //@ts-ignore
+          authenticate({ connector })
+            .then(() => {
+              if (authError) {
+                console.log(authError.message);
+              } else {
+                if (isAuthenticated) {
+                  console.log("LO")
+                }
+              }
+            })
+            .catch(() => {});
           }} title="CONNECT"/>
-        <Button onPress={()=>{
-          connector.sendTransaction({
-            from: connector.accounts[0],
-            to: "0x3a339C136F4482f348e3921EDBa8b8Ebd6931f08",
-            value: "10000000000000000"
-          })
+        <Button onPress={async()=>{
+          // connector.sendTransaction({
+          //   from: connector.accounts[0],
+          //   to: "0x3a339C136F4482f348e3921EDBa8b8Ebd6931f08",
+          //   value: "10000000000000000"
+          // })
         }} title="SEND" />
         <Button onPress={() =>{connector.killSession()}} title="DISCONNECT"/>
         <StatusBar style="auto" />
